80c9d04ce0579bb24cace9313ae6ae398a6216fc
Teyir <60007549+Teyir@users.noreply.github.com>
2022-08-31 11:45:44 +0200

    [IMPROVE] Add 2 placeholders (PAPI)

--- src/main/java/fr/teyir/simpletokens/utils/hooks/PlaceholderAPIExpansions.java
+++ src/main/java/fr/teyir/simpletokens/utils/hooks/PlaceholderAPIExpansions.java
@@ -1,9 +1,14 @@
 package fr.teyir.simpletokens.utils.hooks;
 
 import fr.teyir.simpletokens.SimpleTokens;
+import fr.teyir.simpletokens.db.DBRequest;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.sql.SQLException;
+import java.util.UUID;
 
 public class PlaceholderAPIExpansions extends PlaceholderExpansion {
 
@@ -30,7 +35,35 @@ public class PlaceholderAPIExpansions extends PlaceholderExpansion {
 
     @Override
     public String onPlaceholderRequest(Player player, @NotNull String params) {
-        //TODO PAPI EXPANSIONS
+
+        if (params.equalsIgnoreCase("balance")) {
+            String uuid = player.getUniqueId().toString();
+
+            String value = "";
+
+            try {
+                value += new DBRequest(plugin).getPlayerTokens(uuid);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            return value;
+        } else if (params.startsWith("balance_")) {
+            String pseudo = params.split("(?i)balance_")[1];
+            Player p = Bukkit.getPlayer(pseudo);
+
+            String value = "";
+
+            if (p != null) {
+                try {
+                    value += new DBRequest(plugin).getPlayerTokens(p.getUniqueId().toString());
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            return value;
+        }
+
         return null;
     }
 
